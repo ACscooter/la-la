0179ea6c3be6d78f563408452f50ebc759ec63a4
Antares Chen <antares21chen@gmail.com>
2016-12-25 22:03:14 -0500

    Does this work? I dont know...

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -6,6 +6,9 @@ from auth import auth
 import requests
 import json
 
+GOOGLE_REDIRECT_URI = "/oauth2callback"
+GOOGLE_OAUTH_URL = "https://www.googleapis.com/oauth2/v1/userinfo"
+
 auth = Blueprint('auth', __name__)
 
 oauth = OAuth()
@@ -30,29 +33,25 @@ google = oauth.remote_app('google',
 def index():
     access_token = session.get('access_token')
     if access_token is None:
-        return redirect(url_for('login'))
-    access_token = access_token[0]
+        callback = url_for('authorized', _external=True)
+        return google.authorize(callback=callback)
+    return redirect(url_for('index'))
+    # access_token = access_token[0]
 
-    try:
-        headers = {'Authorization' : "OAuth {0}".format(access_token)}
-        url = "https://www.googleapis.com/oauth2/v1/userinfo"
-        req = requests.get(url, headers=headers)
-        return jsonify(req.json())
-    except requests.exceptions.RequestException:
-        # TODO Log this thing
-        return "YOU FUCKED UP A-A-RON"
-
-@auth.route('/login')
-def login():
-    callback = url_for('authorized', _external=True)
-    return google.authorize(callback=callback)
+    # try:
+    #     headers = {'Authorization' : "OAuth {0}".format(access_token)}
+    #     req = requests.get(GOOGLE_OAUTH_URL, headers=headers)
+    #     return jsonify(req.json())
+    # except requests.exceptions.RequestException:
+    #     # TODO Log this thing
+    #     return "YOU FUCKED UP A-A-RON"
 
 @auth.route(GOOGLE_REDIRECT_URI)
 @google.authorized_handler
 def authorized(resp):
     access_token = resp['access_token']
     session['access_token'] = access_token, ''
-    return redirect(url_for('index'))
+    return redirect(url_for('auth.index'))
 
 @google.tokengetter
 def get_access_token():
